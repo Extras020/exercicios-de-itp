5de9f3b4f7ca0c376b9b2038e810205357960465
Extras020 <120763052+Extras020@users.noreply.github.com>
2025-09-02 17:28:34 -0300

    Update main.c

--- primeiro/main.c
+++ primeiro/main.c
@@ -5,6 +5,14 @@ int main()
 {
     float peso, altura, IMC;
     scanf("%f %f", &peso, &altura);
+    while(peso <= 0){
+        printf("digite um peso válido!!\n");
+        scanf("%f", &peso);
+    }
+    while(altura <= 0){
+        printf("digite uma altura válida\n");
+        scanf("%f", &altura);
+    }
     IMC = peso/(altura*altura);
     printf("IMC = %.2f", IMC);
     return 0;
